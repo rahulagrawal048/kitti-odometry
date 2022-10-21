e807d8e532191418d66bfb5affcc190c74d357c1
Rahul Agrawal <rahulagr@umich.edu>
2022-10-21 19:51:58 -0400

    update

--- get_codewords.m
+++
@@ -1,9 +0,0 @@
-function codewords = get_codewords(feature_all, num_codewords, seed)
-if nargin == 3
-    rng(seed);
-end
-
-%% Write your code here:
-[IDX, C] = kmeans(feature_all, num_codewords,'MaxIter', 500);
-codewords = C;
-end
--- get_features.m
+++
@@ -1,6 +0,0 @@
-function features = get_features(image)
-%% Write your code here:
-points = detectSURFFeatures(image)
-[features, validPoints] = extractFeatures(image,points);
-features = double(features);
-end
